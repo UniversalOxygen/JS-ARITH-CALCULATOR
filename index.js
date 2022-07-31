109d6277a32df5a5fdaf861fb95ffcdcac9635ac
OLAGOKE TUNDE OLUJIDE <olagoketunde600@gmail.com>
2022-07-31 22:58:06 +0800

    Build a basic arithmetic calculator without a frontend

---
+++ index.js
@@ -0,0 +1,42 @@
+// Build a basic arithmetic calculator
+// Without a frontend
+
+let result;
+let operation = prompt('Select ( operation +, -, * or / ): ');
+
+const firstNumber = parseFloat(prompt('First number: '));
+const secondNumber = parseFloat(prompt('Second number: '));
+
+// The calculator should be able to perform basic operations
+// like Addition, Subtraction, Multiplication, & Division
+
+switch(operation) {
+    case '+':
+        result = firstNumber + secondNumber;
+        document.write(`${firstNumber} + ${secondNumber} = ${result}`);
+        console.log(`${firstNumber} + ${secondNumber} = ${result}`);
+		break;
+
+    case '-':
+        result = firstNumber - secondNumber;
+        document.write(`${firstNumber} - ${secondNumber} = ${result}`);
+        console.log(`${firstNumber} - ${secondNumber} = ${result}`);
+        break;
+
+    case '*':
+        result = firstNumber * secondNumber;
+        document.write(`${firstNumber} * ${secondNumber} = ${result}`);
+        console.log(`${firstNumber} * ${secondNumber} = ${result}`);
+        break;
+
+    case '/':
+        result = firstNumber / secondNumber;
+        document.write(`${firstNumber} / ${secondNumber} = ${result}`);
+        console.log(`${firstNumber} / ${secondNumber} = ${result}`);
+        break;
+
+    default:
+        document.write('Invalid operation');
+        console.log('Invalid operation');
+        break;
+}
